805ab1566c9498781433aca7735acbc3777e074e
ChristopherSO <csuarezo@ucenfotec.ac.cr>
2015-01-06 16:19:11 -0600

    Tomando la última versión de la carpeta UI

---
+++ UI/Pantallas de la aplicacion/Modulo de becas/popups/frmAgregarTipoBeca.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAgregarTipoBeca
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnlContenidoPrincipal = New System.Windows.Forms.Panel()
+        Me.pnlAgregarTipoDeBeca = New System.Windows.Forms.Panel()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.txtDescripcion = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.lblNombreTipoBeca = New System.Windows.Forms.Label()
+        Me.lblSubTitulo = New System.Windows.Forms.Label()
+        Me.ckbSocioeconomico = New System.Windows.Forms.CheckBox()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.lblCantidad = New System.Windows.Forms.Label()
+        Me.pnlContenidoPrincipal.SuspendLayout()
+        Me.pnlAgregarTipoDeBeca.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlContenidoPrincipal
+        '
+        Me.pnlContenidoPrincipal.Controls.Add(Me.pnlAgregarTipoDeBeca)
+        Me.pnlContenidoPrincipal.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlContenidoPrincipal.Location = New System.Drawing.Point(0, 0)
+        Me.pnlContenidoPrincipal.Name = "pnlContenidoPrincipal"
+        Me.pnlContenidoPrincipal.Size = New System.Drawing.Size(604, 411)
+        Me.pnlContenidoPrincipal.TabIndex = 0
+        '
+        'pnlAgregarTipoDeBeca
+        '
+        Me.pnlAgregarTipoDeBeca.Controls.Add(Me.ckbSocioeconomico)
+        Me.pnlAgregarTipoDeBeca.Controls.Add(Me.txtCantidad)
+        Me.pnlAgregarTipoDeBeca.Controls.Add(Me.lblCantidad)
+        Me.pnlAgregarTipoDeBeca.Controls.Add(Me.btnCancelar)
+        Me.pnlAgregarTipoDeBeca.Controls.Add(Me.btnGuardar)
+        Me.pnlAgregarTipoDeBeca.Controls.Add(Me.txtDescripcion)
+        Me.pnlAgregarTipoDeBeca.Controls.Add(Me.Label1)
+        Me.pnlAgregarTipoDeBeca.Controls.Add(Me.txtNombre)
+        Me.pnlAgregarTipoDeBeca.Controls.Add(Me.lblNombreTipoBeca)
+        Me.pnlAgregarTipoDeBeca.Controls.Add(Me.lblSubTitulo)
+        Me.pnlAgregarTipoDeBeca.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlAgregarTipoDeBeca.Location = New System.Drawing.Point(0, 0)
+        Me.pnlAgregarTipoDeBeca.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlAgregarTipoDeBeca.Name = "pnlAgregarTipoDeBeca"
+        Me.pnlAgregarTipoDeBeca.Size = New System.Drawing.Size(604, 411)
+        Me.pnlAgregarTipoDeBeca.TabIndex = 3
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.BackColor = System.Drawing.Color.DimGray
+        Me.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCancelar.ForeColor = System.Drawing.Color.White
+        Me.btnCancelar.Location = New System.Drawing.Point(406, 314)
+        Me.btnCancelar.Margin = New System.Windows.Forms.Padding(135, 10, 3, 15)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(150, 47)
+        Me.btnCancelar.TabIndex = 13
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = False
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.BackColor = System.Drawing.Color.DodgerBlue
+        Me.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnGuardar.ForeColor = System.Drawing.Color.White
+        Me.btnGuardar.Location = New System.Drawing.Point(233, 314)
+        Me.btnGuardar.Margin = New System.Windows.Forms.Padding(135, 10, 3, 15)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(150, 47)
+        Me.btnGuardar.TabIndex = 12
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = False
+        '
+        'txtDescripcion
+        '
+        Me.txtDescripcion.Location = New System.Drawing.Point(135, 107)
+        Me.txtDescripcion.Multiline = True
+        Me.txtDescripcion.Name = "txtDescripcion"
+        Me.txtDescripcion.Size = New System.Drawing.Size(421, 106)
+        Me.txtDescripcion.TabIndex = 7
+        Me.txtDescripcion.Tag = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel)
+        Me.Label1.Location = New System.Drawing.Point(21, 112)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 17)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Descripción:"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(135, 69)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(421, 26)
+        Me.txtNombre.TabIndex = 5
+        Me.txtNombre.Tag = ""
+        '
+        'lblNombreTipoBeca
+        '
+        Me.lblNombreTipoBeca.AutoSize = True
+        Me.lblNombreTipoBeca.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel)
+        Me.lblNombreTipoBeca.Location = New System.Drawing.Point(21, 74)
+        Me.lblNombreTipoBeca.Name = "lblNombreTipoBeca"
+        Me.lblNombreTipoBeca.Size = New System.Drawing.Size(62, 17)
+        Me.lblNombreTipoBeca.TabIndex = 4
+        Me.lblNombreTipoBeca.Text = "Nombre:"
+        '
+        'lblSubTitulo
+        '
+        Me.lblSubTitulo.AutoSize = True
+        Me.lblSubTitulo.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel)
+        Me.lblSubTitulo.Location = New System.Drawing.Point(15, 19)
+        Me.lblSubTitulo.Margin = New System.Windows.Forms.Padding(0)
+        Me.lblSubTitulo.Name = "lblSubTitulo"
+        Me.lblSubTitulo.Size = New System.Drawing.Size(238, 29)
+        Me.lblSubTitulo.TabIndex = 3
+        Me.lblSubTitulo.Text = "Agregar tipo de beca"
+        '
+        'ckbSocioeconomico
+        '
+        Me.ckbSocioeconomico.AutoSize = True
+        Me.ckbSocioeconomico.Location = New System.Drawing.Point(135, 265)
+        Me.ckbSocioeconomico.Name = "ckbSocioeconomico"
+        Me.ckbSocioeconomico.Size = New System.Drawing.Size(145, 24)
+        Me.ckbSocioeconomico.TabIndex = 17
+        Me.ckbSocioeconomico.Text = "Socioeconomico"
+        Me.ckbSocioeconomico.UseVisualStyleBackColor = True
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(135, 223)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(421, 26)
+        Me.txtCantidad.TabIndex = 16
+        Me.txtCantidad.Tag = "txtnum"
+        '
+        'lblCantidad
+        '
+        Me.lblCantidad.AutoSize = True
+        Me.lblCantidad.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel)
+        Me.lblCantidad.Location = New System.Drawing.Point(21, 232)
+        Me.lblCantidad.Name = "lblCantidad"
+        Me.lblCantidad.Size = New System.Drawing.Size(68, 17)
+        Me.lblCantidad.TabIndex = 15
+        Me.lblCantidad.Text = "Cantidad:"
+        '
+        'frmAgregarTipoBeca
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(604, 411)
+        Me.Controls.Add(Me.pnlContenidoPrincipal)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "frmAgregarTipoBeca"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Agregar tipo de beca"
+        Me.pnlContenidoPrincipal.ResumeLayout(False)
+        Me.pnlAgregarTipoDeBeca.ResumeLayout(False)
+        Me.pnlAgregarTipoDeBeca.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents pnlContenidoPrincipal As System.Windows.Forms.Panel
+    Friend WithEvents pnlAgregarTipoDeBeca As System.Windows.Forms.Panel
+    Friend WithEvents txtDescripcion As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtNombre As System.Windows.Forms.TextBox
+    Friend WithEvents lblNombreTipoBeca As System.Windows.Forms.Label
+    Friend WithEvents lblSubTitulo As System.Windows.Forms.Label
+    Friend WithEvents btnGuardar As System.Windows.Forms.Button
+    Friend WithEvents btnCancelar As System.Windows.Forms.Button
+    Friend WithEvents ckbSocioeconomico As System.Windows.Forms.CheckBox
+    Friend WithEvents txtCantidad As System.Windows.Forms.TextBox
+    Friend WithEvents lblCantidad As System.Windows.Forms.Label
+End Class
